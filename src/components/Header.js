a75a9db976e3884dcbfd6f088615f57cc85cb813
btopolnjak <boristopolnjak@Boriss-MacBook-Pro.local>
2022-01-09 20:35:51 +0100

    fixed header date

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,11 @@ export default function Header({ headerMenuShow, setHeaderMenuShow }) {
     const currentDate = new Date();
     const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
     return (
         <header>
             <div>
                 <h1>{weekday[currentDate.getDay()]}</h1>
-                <h2>{currentDate.getDay()}. {month[currentDate.getMonth()]} {currentDate.getFullYear()}.</h2>
+                <h2>{currentDate.getDate()}. {month[currentDate.getMonth()]} {currentDate.getFullYear()}.</h2>
             </div>
             <div
                 style={headerMenuShow ? {color: "#0080FF"} : {color: "white"}}
